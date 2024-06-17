36cc2a25be9d3c7e3a75f93df9a118be270ae211
onasheep <inabass@naver.com>
2024-06-18 00:33:34 +0900

    feat : fragment shader to linking shader
    fragment shader to linking shader by using shaderProgram

--- OpenGL_Project/OpenGL_Project/Sources/main.cpp
+++ OpenGL_Project/OpenGL_Project/Sources/main.cpp
@@ -7,12 +7,19 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void proecessInput(GLFWwindow* window);
 
 
-const char* vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 "	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\n";
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 02.f, 1.0f);\n"
+"}\n\0";
 
 int main()
 {
@@ -62,6 +69,7 @@ int main()
 		glfwPollEvents();
 	}
 
+	// vertex shader
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
@@ -70,12 +78,42 @@ int main()
 	int success;
 	char infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
 	if (!success)
 	{
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::VERTEXT::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
+
+	// fragment shader
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// for checking fragment shader compile success
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// link shader
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 
 	float vertices[] =
