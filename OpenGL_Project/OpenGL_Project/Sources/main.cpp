d13a7808c58831c30a9fb92394fd5409ee76b3b0
onasheep <inabass@naver.com>
2024-06-20 00:00:03 +0900

    feat : draw first triangle
    use VAO to draw triangle

--- OpenGL_Project/OpenGL_Project/Sources/main.cpp
+++ OpenGL_Project/OpenGL_Project/Sources/main.cpp
@@ -15,10 +15,10 @@ const char *vertexShaderSource = "#version 330 core\n"
 "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor\n"
+"out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 0.5f, 02.f, 1.0f);\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\n\0";
 
 int main()
@@ -39,6 +39,7 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -46,28 +47,9 @@ int main()
 		return -1;
 	}
 
-	glViewport(0, 0, 800, 600);
 
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// render loop
-	while (!glfwWindowShouldClose(window))
-	{
 
-		// inpyt
-		proecessInput(window);
-
-		// rendering commands here
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		// ....
-		//
-		
-
-		// check all call events and swap the buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
 
 	// vertex shader
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -110,12 +92,12 @@ int main()
 		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
 	}
 
-	glUseProgram(shaderProgram);
 
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-
+	// set up vertex data (and buffer(s)) and configure vertex attribute
+	// ----------------------------------------------------------
 	float vertices[] =
 	{
 		-0.5f, -0.5f, 0.0f,
@@ -124,12 +106,52 @@ int main()
 	};
 
 
-	unsigned int VBO;
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	// bind the Vertex Array Obejct first, then bind and set vertext buffer(s), and then configure vertext attribute(s).
+	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
+	// note that this is allowed, the call to glVertexAttribPointer resistered VBO as the vertext attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+
+		// inpyt
+		proecessInput(window);
+
+		// rendering commands here
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		// draw our first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO); // // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// glBindVertexArray(0); // no need to unbind it every time 
+
+		// glfw: swap buffers and poll IO events (key pressed/released, mouse moved etc.)
+		// 
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+	// optional: de-allocate all resources once they've outliverd their purpose:
+	// ---------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 
 
